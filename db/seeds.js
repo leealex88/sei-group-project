581ecc98249ad0f3ee312f16aac00db83dc17332
Lily Keeling <lily.keeling@gmail.com>
2019-07-05 11:59:03 +0100

    added router and controllers

--- db/seeds.js
+++ db/seeds.js
@@ -11,7 +11,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'game',
           eventName: 'Bridge Night',
-          date: new Date('07/25/2019'),
+          date: new Date('07/04/2019'),
           location: 'Camberwell',
           description: '',
           schedule: '',
@@ -37,7 +37,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'picnic',
           name: 'non-alcoholic picnic',
-          date: new Date('07/25/2019'),
+          date: new Date('07/10/2019'),
           fixed: false,
           location: 'Peckham Rye',
           description: 'Just a nice (sober!) afternoon (hopefully!) sitting in the sun.',
@@ -64,7 +64,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Drinks',
           name: 'After Work Drink and Chat',
-          date: new Date('07/25/2019'),
+          date: new Date('07/28/2019'),
           fixed: true,
           location: 'The City',
           description: 'Please come have a drink and a chat with me, I\'d appreciate the opportunity to have a good moan and I can offer a listening ear too',
@@ -91,7 +91,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Yoga',
           name: 'Morning Yoga Session',
-          date: new Date('07/25/2019'),
+          date: new Date('07/17/2019'),
           fixed: true ,
           location: 'Brixton',
           description: 'Relaxed pre-work yoga session in my local park',
@@ -118,7 +118,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Tea and Cake',
           name: 'An informal afternoon tea at my lovely home',
-          date: new Date('07/25/2019'),
+          date: new Date('07/12/2019'),
           fixed: false ,
           location: 'Poplar',
           description: 'I love baking and always want a chance to feed people cake!',
@@ -145,7 +145,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Shopping',
           name: 'Vintage Clothes Shopping- Brick Lane',
-          date: new Date('08/25/2019'),
+          date: new Date('08/01/2019'),
           fixed: true ,
           location: 'Brick Lane',
           description: 'Need some company for a bit of fun vintage shopping, desperately need a very ridiculous new outfit to wear to a super hipster party I\'m going to and don\'t want to shop alone',
@@ -171,7 +171,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Language Exchange',
           name: 'My French for Your English',
-          date: new Date('08/25/2019'),
+          date: new Date('08/01/2019'),
           fixed: true ,
           location: 'Dulwich',
           description: 'Frenchy looking for a funny but serious conversation with interesting English person. It is perfect if you want to learn French too',
@@ -197,7 +197,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Breakfast',
           name: 'Greasy Fry-Up in my Local Caff',
-          date: new Date('08/25/2019'),
+          date: new Date('08/02/2019'),
           fixed: false ,
           location: 'Tower Hamlets',
           description: 'I hate eating breakfast alone but this place is the best and I can\'t get enough of their full English, join me for brekky and have your mind blown. Best sausage in London',
@@ -223,7 +223,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Film',
           name: 'Screening of Brief Encounter',
-          date: new Date('08/25/2019'),
+          date: new Date('08/05/2019'),
           fixed: true ,
           location: 'Shoreditch',
           description: 'This is one of my very favourite films but I hate going to the cinema alone. It\'s such a lovely and romantic bit of cinema, please don\'t let the fact that is old put you off even if that\'t not normally your thing.',
@@ -250,7 +250,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Dog Walking',
           name: '(Long) Sunday Afternoon Dog Walk',
-          date: new Date('08/25/2019'),
+          date: new Date('08/09/2019'),
           fixed: true ,
           location: 'Richmond',
           description: 'Join me and my beautiful Chow Chow Lola for a long dong walk in Richmond park, I love a good deep chat about anything or everything (or nothing much!) and I\'m an excellent listener. Lola is very social too but does have a bit of a problem with excitable/jumpy small dogs.',
@@ -276,7 +276,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Ping-Pong',
           name: 'Very serious Ping-Pong Tournament',
-          date: new Date('08/25/2019'),
+          date: new Date('08/09/2019'),
           fixed: true ,
           location: 'Kilburn',
           description: 'This tournament will not be the faint harded! I take my ping pong (aka table tennis, aka WHIFF WHAFF) exceedingly seriously so please don\'t bother RSVP-ing if you don\'t even know the TTA\'s official rules or don\'t own your own bat!!!!!',
@@ -302,7 +302,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Ping-Pong',
           name: 'Very serious Ping-Pong Tournament',
-          date: new Date('08/25/2019'),
+          date: new Date('08/13/2019'),
           fixed: true ,
           location: 'Kilburn',
           description: 'This tournament will not be the faint harded! I take my ping pong (aka table tennis, aka WHIFF WHAFF) exceedingly seriously so please don\'t bother RSVP-ing if you don\'t even know the TTA\'s official rules or don\'t own your own bat!!!!!',
@@ -328,7 +328,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Jam Session',
           name: 'Casual (indie rock) Jam Session',
-          date: new Date('08/25/2019'),
+          date: new Date('08/15/2019'),
           fixed: true ,
           location: 'Dalston',
           description: 'I play guitar and I\'ve got a big garage and a drum kit, would love to spend an afternoon jamming (and maybe drinking beer!) with a few fellow music lovers and your instruments',
@@ -354,7 +354,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Game',
           name: 'Dungeons and Dragons',
-          date: new Date('08/25/2019'),
+          date: new Date('08/17/2019'),
           fixed: false ,
           location: 'Islington',
           description: 'Would love to form a new group and get back into this, I\'m an experienced DM (dungeon master) but I welcome all levels and I\'m very willing to teach!',
@@ -380,7 +380,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Make-over',
           name: 'Make-up session with Prosecco and Cheese',
-          date: new Date('08/25/2019'),
+          date: new Date('08/12/2019'),
           fixed: false ,
           location: 'Camden Town',
           description: 'Hi :), I\'ve just completed a course in doing full face make-up and I would love a chance to practice my skills! I will give you a whole new dramatic look and feed you prosecco too!!!!!!! Men and women both welcome, you just must be happy to have lots of stuff put on your face.',
@@ -406,7 +406,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Watching Sport',
           name: 'Liverpool V. Bangor',
-          date: new Date('08/25/2019'),
+          date: new Date('08/19/2019'),
           fixed: false ,
           location: 'Holborm',
           description: 'Come round my gaff and watch the footie, I\'m a Bangor man (come on you Bangers!) but all allegances welcome. It\'ll be well macho (lads, lads, lads) but birds welcome so long as you\'ve got banter',
@@ -432,7 +432,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Running',
           name: 'Morning Thames Run',
-          date: new Date('08/25/2019'),
+          date: new Date('08/28/2019'),
           fixed: false ,
           location: 'Tower Bridge',
           description: 'Join me for a Thames loop going from Tower Bridge to Westminster, I\'m not a fast runner and would prefer it if you\'d keep pace with me as that is the whole point of having company!',
@@ -458,7 +458,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Game',
           name: 'Chess Game',
-          date: new Date('08/25/2019'),
+          date: new Date('08/11/2019'),
           fixed: false ,
           location: 'Clerkenwell',
           description: 'I have not played in quite some time but would love to get back into the game, I used to be a very serious player but am undoubtedly somewhat oxidised (rusty).',
@@ -484,7 +484,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Meal',
           name: 'Dinner at Moro',
-          date: new Date('08/25/2019'),
+          date: new Date('08/10/2019'),
           fixed: false ,
           location: 'Faringdon',
           description: 'I have been wanting to dine at Moro for quite some time but haven\'t mustered the courage to go alone. The food is said to be muy delicioso.',
@@ -510,7 +510,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Coffee',
           name: 'Casual cup of coffee and a bit of a chat',
-          date: new Date('08/25/2019'),
+          date: new Date('08/13/2019'),
           fixed: false ,
           location: 'Greenwich',
           description: 'I am disabled and don\'t get out much, I\'ll happily buy you a coffee and a slice of cake if you fancy sitting with me for a few hours and just having a good natter',
@@ -536,7 +536,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Political Canvassing',
           name: 'Door knocking for the Greens',
-          date: new Date('09/25/2019'),
+          date: new Date('09/16/2019'),
           fixed: false ,
           location: 'Chelsea',
           description: 'I am a Green party member but you don\'t need to be offically signed up, if you agree with our values and want to keep me company while we do a bit of campaigning then I\'d very much appreciate it',
@@ -562,7 +562,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Meal',
           name: 'Mexican Night!',
-          date: new Date('09/25/2019'),
+          date: new Date('09/18/2019'),
           fixed: false ,
           location: 'Hackney',
           description: 'I love to cook and have a serious craving for burittos but no-one to cook for, there will be plenty of food and even more plenty of MARGARITA!',
@@ -588,7 +588,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Conversation',
           name: 'A nice chat',
-          date: new Date('09/25/2019'),
+          date: new Date('09/01/2019'),
           fixed: false ,
           location: 'Haringey',
           description: 'I am 84 years old and house bound, I have all of my basic needs met but do miss having somebody to talk to. I have lived a full life and have a few good stories to tell. I also like to think that I have learned a thing or two and give sensible advice if you would like to ask me for my thoughts on anything that is troubling you.',
@@ -614,7 +614,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Charity',
           name: 'Help the Homeless',
-          date: new Date('07/25/2019'),
+          date: new Date('07/02/2019'),
           fixed: false ,
           location: 'The City',
           description: 'I work for a small charity that aims to help those sleeping rough in London. A big part of this work is just getting out there and talking to folk. We need to understand what happened to these people and what they need if we want to solve this problem. If you fancy helping out and joining me on a night out and about that\'d be great.',
@@ -640,7 +640,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Charity',
           name: 'Help the Homeless',
-          date: new Date('07/25/2019'),
+          date: new Date('07/10/2019'),
           fixed: false ,
           location: 'The City',
           description: 'I work for a small charity that aims to help those sleeping rough in London. A big part of this work is just getting out there and talking to folk. We need to understand what happened to these people and what they need if we want to solve this problem. If you fancy helping out and joining me on a night out and about that\'d be great.',
@@ -666,7 +666,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Play Date',
           name: 'Park Trip with Small People',
-          date: new Date('07/25/2019'),
+          date: new Date('07/11/2019'),
           fixed: false ,
           location: 'Southwark Park',
           description: 'I\'m a stay at home Dad and have a bit of problem when it comes to doing things with the little ones in the day as I have no SAHP friends! It\'d be great to have a bit of company. My small people are 3, 2 and 5 months (I KNOW!).',
@@ -692,7 +692,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Gardening',
           name: 'Gardening in the Community Gardens',
-          date: new Date('07/25/2019'),
+          date: new Date('07/02/2019'),
           fixed: false ,
           location: 'Hackney',
           description: 'Our lovely little local garden is looking a bit overgrown, join me with your clippers and let\'s get pruning! I am more than happy give guidance and advice to anyone inexperienced at caring for plants and stuff.',
@@ -718,7 +718,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Meal',
           name: 'Family Friendly BBQ',
-          date: new Date('07/25/2019'),
+          date: new Date('07/13/2019'),
           fixed: false ,
           location: 'Hyde Park',
           description: 'We are planning a family day out in the park with lots of drinks, nibbles, fun, games and a BBQ! We are a very social fun loving familyand we are keen to make local friends as we are brand new to the area.',
@@ -744,7 +744,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Art Gallery',
           name: 'Natalia Goncharova Exhibtion',
-          date: new Date('07/25/2019'),
+          date: new Date('07/03/2019'),
           fixed: false ,
           location: 'Tate Modern',
           description: 'Would love some company for this. She is absolutely one of my favourite ever artists and if you don\'t know her work or a lot about her please check the link and if you are interested I will be so happy to share my knowledge with you (she was my dissertation topic)',
@@ -770,7 +770,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'Art Gallery',
           name: 'Natalia Goncharova Exhibtion',
-          date: new Date('07/25/2019'),
+          date: new Date('07/05/2019'),
           fixed: false ,
           location: 'Tate Modern',
           description: 'Would love some company for this. She is absolutely one of my favourite ever artists and if you don\'t know her work or a lot about her please check the link and if you are interested I will be so happy to share my knowledge with you (she was my dissertation topic)',
@@ -796,7 +796,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'British Museum Visit',
           name: 'Day at the British Museum',
-          date: new Date('07/25/2019'),
+          date: new Date('07/23/2019'),
           fixed: false ,
           location: 'Bloomsbury',
           description: 'Somehow I\'ve never visited and it seems so very silly that I have not, I would like some company as I always find it more fun to look at stuff when I can talk about the stuff I am looking at (without looking unhinged)',
@@ -822,7 +822,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true }, (err, d
         {
           eventType: 'British Museum Visit',
           name: 'Day at the British Museum',
-          date: new Date('07/25/2019'),
+          date: new Date('07/12/2019'),
           fixed: false ,
           location: 'Bloomsbury',
           description: 'Somehow I\'ve never visited and it seems so very silly that I have not, I would like some company as I always find it more fun to look at stuff when I can talk about the stuff I am looking at (without looking unhinged)',
