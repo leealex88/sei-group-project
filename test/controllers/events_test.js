f9c06a500b1886beb18d5b05558b608fb2ef83d1
CPrich905 <charlie.prichard@gmail.com>
2019-07-10 14:09:52 +0100

    tests for get & post events working

--- test/controllers/events_test.js
+++ test/controllers/events_test.js
@@ -4,12 +4,50 @@
 require('../spec_helper')
 
 const Event = require('../../models/event')
+const User = require('../../models/user')
+const { secret } = require('../../config/environment')
+
+const jwt = require('jsonwebtoken')
+
+const eventData = {
+  eventType: 'Dog Walking',
+  eventName: '(Long) Sunday Afternoon Dog Walk',
+  date: new Date('08/09/2019'),
+  fixed: true ,
+  location: 'Richmond',
+  description: 'Join me and my beautiful Chow Chow Lola for a long dong walk in Richmond park, I love a good deep chat about anything or everything (or nothing much!) and I\'m an excellent listener. Lola is very social too but does have a bit of a problem with excitable/jumpy small dogs.',
+  startTime: '1200',
+  endTime: '1500',
+  cost: 0,
+  whatToBring: 'The usual dog walking things! Maybe some money for a drink in a dog friendly pub after if you\re up for it',
+  maxSize: 5,
+  minSize: 2,
+  tags: ['dogs', 'walking', 'talking', 'listening'],
+  skillLevel: '',
+  user: '5d249b40dd7fa1eab1ff08ad'
+}
+
+let token
 
 describe('Event tests', () => {
 
   beforeEach(done => {
     Event.collection.deleteMany()
-    done()
+    Event.create(eventData)
+      .then(() => User.deleteMany({}))
+      .then(() => User.create({
+        username: 'test',
+        password: 'test',
+        email: 'test@mail',
+        bio: 'Definitely a scientist, apparently a doctor (though no one is entirely sure what type of doctor), probably insane. Also maybe a clone of Hitler.',
+        passwordConfirmation: 'test'
+      }))
+      .then(user => {
+        token = jwt.sign({ sub: user._id }, secret, { expiresIn: '9h' } )
+        done()
+      })
+
+      .catch(done)
   })
 
   afterEach(done => {
@@ -17,29 +55,8 @@ describe('Event tests', () => {
     done()
   })
 
+  //test for event index
   describe('GET /api/events', () => {
-
-    beforeEach(done => {
-      Event.create({
-        eventType: 'Dog Walking',
-        eventName: '(Long) Sunday Afternoon Dog Walk',
-        date: new Date('08/09/2019'),
-        fixed: true ,
-        location: 'Richmond',
-        description: 'Join me and my beautiful Chow Chow Lola for a long dong walk in Richmond park, I love a good deep chat about anything or everything (or nothing much!) and I\'m an excellent listener. Lola is very social too but does have a bit of a problem with excitable/jumpy small dogs.',
-        startTime: '1200',
-        endTime: '1500',
-        cost: 0,
-        whatToBring: 'The usual dog walking things! Maybe some money for a drink in a dog friendly pub after if you\re up for it',
-        maxSize: 5,
-        minSize: 2,
-        tags: ['dogs', 'walking', 'talking', 'listening'],
-        skillLevel: '',
-        user: '5d249b40dd7fa1eab1ff08ad'
-      })
-        .then(() => done())
-        .catch(done)
-    })
 
     it('should return a 200 response', done => {
       api
@@ -85,21 +102,12 @@ describe('Event tests', () => {
               'fixed',
               'location',
               'description',
-              'schedule',
               'startTime',
               'endTime',
-              'tickets',
-              'ticketLink',
-              'eventLink',
-              'preparation',
               'cost',
-              'catered',
-              'provided',
               'whatToBring',
               'maxSize',
               'minSize',
-              'idealGroupSize',
-              'privateInfo',
               'partyImage',
               'tags',
               'skillLevel',
@@ -113,4 +121,39 @@ describe('Event tests', () => {
         })
     })
   })
+
+  describe('POST /api/events', () => {
+
+    it('should return a 201 response', done =>{
+      api
+        .post('/api/events')
+        .set({ 'Accept': 'application/json', 'Authorization': `Bearer ${token}` })
+        .send(eventData)
+        .expect(201, done)
+    })
+
+
+    it('should create an event', done => {
+      api
+        .get('/api/events')
+        .set({ 'Accept': 'application/json', 'Authorization': `Bearer ${token}` })
+        .send({ eventData })
+        .end((err, res) => {
+          const event1 = res.body
+
+          expect(event1)
+          expect(res.body)
+            .and.be.an('array')
+            .and.have.property(0)
+            .to.have.property('eventType')
+            .and.to.be.a('string')
+          done()
+        })
+    })
+  })
+
 })
+
+//test POST request (create event)
+//test GET specific event
+//test index
