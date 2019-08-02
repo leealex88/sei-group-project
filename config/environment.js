1b85e29c343e4fd45c12c723cca01ff9117661f0
CPrich905 <charlie.prichard@gmail.com>
2019-08-02 14:37:58 +0100

    ready for deploy test

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGODB_URI || '//localhost:27017/strangerthings'
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/strangerthings'
 const secret = process.env.SECRET || 'secreto'
 
 //FOR TESTING
--- index.js
+++ index.js
@@ -1,24 +1,27 @@
+require('dotenv').config()
 const bodyParser = require('body-parser')
 const express = require('express')
 const mongoose = require('mongoose')
-require('dotenv').config()
 const app = express()
 const logger = require('./lib/logger')
 const router = require('./config/router')
 const { dbURI, port } = require('./config/environment')
-
 const errorHandler = require('./lib/errorHandler')
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true })
 
+app.use(express.static(`${__dirname}/dist`))
+
 app.use(bodyParser.json())
+
 app.use('/api', router)
+
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 
 app.use(errorHandler)
 
 app.use(logger)
 
-app.listen(process.env.PORT, () => console.log(`Running on port ${process.env.PORT}`))
 app.listen(port, () => console.log(`App is listening on port ${port}`))
 
 module.exports = app
